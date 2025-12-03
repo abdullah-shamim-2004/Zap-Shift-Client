29d3fff1905cf88a0e131b4cf536f1a21689cd6f
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2025-12-03 13:00:12 +0600

    Added Interceptor with firebase access token.

--- src/Hooks/useSecure.js
+++ src/Hooks/useSecure.js
@@ -1,10 +1,50 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
   headers: { "Content-Type": "application/json" },
 });
 const useSecure = () => {
+  const { user, UserSignOut } = useAuth();
+  const token = user.accessToken;
+  const navigate = useNavigate();
+
+  // console.log(user.accessToken);
+
+  useEffect(() => {
+    const requestInterceptor = axiosSecure.interceptors.request.use(
+      (config) => {
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+    //response interceptor
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          UserSignOut().then(() => {
+            navigate("/auth/login");
+          });
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(requestInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [token, UserSignOut, navigate]);
   return axiosSecure;
 };
 
--- src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/DashBoard/PaymentHistory/PaymentHistory.jsx
@@ -10,12 +10,46 @@ const PaymentHistory = () => {
     queryKey: ["payments", user.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/payments?email=${user.email}`);
-      return res.data;
+      return res.data.data;
     },
   });
+  // console.log(payments);
+
   return (
-    <div>
+    <div className="p-5">
       <h2 className="text-5xl font-bold">Payment History {payments.length}</h2>
+      <div>
+        {payments.length === 0 ? (
+          <div className="flex flex-col justify-center items-center h-[60vh]">
+            <h2>No Payment History</h2>
+          </div>
+        ) : (
+          <div className="overflow-x-auto">
+            <table className="table table-zebra">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Index</th>
+                  <th>Name</th>
+                  <th>Amount</th>
+                  <th>Tranjection Id</th>
+                </tr>
+              </thead>
+              <tbody>
+                {payments.map((payment, index) => (
+                  <tr key={index}>
+                    <th>{index + 1}</th>
+                    <td>{payment.parcelName}</td>
+                    <td>{payment.amount}</td>
+                    <td>{payment.transactionId}</td>
+                  </tr>
+                ))}
+                {/* row 1 */}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
